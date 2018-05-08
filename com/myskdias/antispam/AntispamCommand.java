0236350402dbbbf61790a2aad1bab9730f982f68
Myskdias <38868302+Myskdias@users.noreply.github.com>
2018-05-08 21:56:46 +0200

    Add files via upload

---
+++ com/myskdias/antispam/AntispamCommand.java
@@ -0,0 +1,69 @@
+package com.myskdias.antispam;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+public class AntispamCommand implements CommandExecutor {
+
+	public Sky sky;
+	
+	public AntispamCommand() {
+		sky = Sky.getInstance();
+	}
+	
+	@Override
+	public boolean onCommand(CommandSender sender, Command cmd, String arg2, String[] args) {
+		if(args.length == 0) {
+			if(sender.hasPermission(sky.permTurnOff) || sender.hasPermission("antispam.*") || sender.isOp()) {
+				if(sky.on) {
+					sky.on = false;
+					sender.sendMessage("§6L'antispam est désactivé");
+				} else {
+					sky.on = true;
+					sender.sendMessage("§6L'antispam est activé");
+				}
+				return false;
+			}
+		} else if(args.length == 3) {
+			if(sender.hasPermission(sky.permModify) || sender.hasPermission("antispam.*") || sender.isOp()) {
+				if(args[0].equalsIgnoreCase("set")) {
+					if(args[1].equalsIgnoreCase("spamTime")) {
+						try {
+							int a = Integer.parseInt(args[2]);
+							sky.getConfig().set("plugin.spamTime", a);
+							return false;
+						} catch(NumberFormatException e) {
+							sender.sendMessage("§6Please enter a valid number");
+							return false;
+						}
+					} else if(args[1].equalsIgnoreCase("messageCooldown")) {
+						try {
+							int a = Integer.parseInt(args[2]);
+							sky.getConfig().set("plugin.messageCooldown", a);
+							return false;
+						} catch(NumberFormatException e) {
+							sender.sendMessage("§6Please enter a valid number");
+							return false;
+						}
+					} else if(args[1].equalsIgnoreCase("messageLimite")) {
+						try {
+							int a = Integer.parseInt(args[2]);
+							sky.getConfig().set("plugin.messageLimite", a);
+							return false;
+						} catch(NumberFormatException e) {
+							sender.sendMessage("§6Please enter a valid number");
+							return false;
+						}
+					}
+				}
+				sender.sendMessage("§6/antispam set + [args] + time");
+				sender.sendMessage("§6args = messageLimite");
+				sender.sendMessage("§6args = messageCooldown");
+				sender.sendMessage("§6args = spamTime");
+			}
+		}
+		return true;
+	}
+
+}
---
+++ com/myskdias/antispam/ChatListener.java
@@ -0,0 +1,79 @@
+package com.myskdias.antispam;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
+
+public class ChatListener implements Listener {
+
+	public HashMap<String, Long> cooldown = new HashMap<>();
+	public ArrayList<Long> chatTime;
+	public Sky sky;
+	public long timeEnd;
+	public boolean b;
+	
+	public ChatListener() {
+		sky = Sky.getInstance();
+		timeEnd = 0;
+		b = false;
+		chatTime = new ArrayList<>(sky.messageLimite);
+	}
+	
+	@EventHandler(priority = EventPriority.HIGHEST)
+	public void onChat(AsyncPlayerChatEvent e) {
+		Player p = e.getPlayer();
+		if(p.hasPermission(sky.permBypass) || p.isOp()) {
+			return;
+		}
+		long timeMillis = System.currentTimeMillis();
+		//chat Limité
+		if(timeMillis < timeEnd) {
+			Long l = cooldown.get(p.getName());
+			if(l == null) {
+				cooldown.put(p.getName(), timeMillis + sky.messageCooldown);
+				updateChatTime(timeMillis);
+				return;
+			}
+			if(timeMillis < l) {
+				e.setCancelled(true);
+				if(!sky.messageError.isEmpty()) {
+					p.sendMessage(sky.messageError.replace("%i", "" + (int) ((l - timeMillis)/1000)));
+				}
+				return;
+			} else {
+				updateChatTime(timeMillis);
+				cooldown.put(p.getName(), timeMillis + sky.messageCooldown);
+				return;
+			}
+		}
+		
+		if(b) {
+			b = false;
+			if(!sky.messageDesactivation.isEmpty())
+				Bukkit.broadcastMessage(sky.messageDesactivation);
+		}
+		updateChatTime(timeMillis);
+	}
+	
+	public void updateChatTime(long timeMillis) {
+		Long base = chatTime.remove(0);
+		chatTime.add(sky.messageLimite - 1, timeMillis);
+		if(base != null) {
+			if(timeMillis - base < 60_000) {
+				timeEnd = timeMillis + sky.spamTime;
+				if(!b) {
+					b = true;
+					if(!sky.messageActivation.isEmpty())
+						Bukkit.broadcastMessage(sky.messageActivation.replace("&i", ""+(int)sky.messageCooldown / 1000));
+				}
+			}
+		}
+	}
+	
+}
