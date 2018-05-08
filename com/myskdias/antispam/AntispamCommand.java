0f2b3835211b2c5be2851658de65628c67a4ce99
Myskdias <38868302+Myskdias@users.noreply.github.com>
2018-05-08 22:19:16 +0200

    Update AntispamCommand.java

--- com/myskdias/antispam/AntispamCommand.java
+++ com/myskdias/antispam/AntispamCommand.java
@@ -18,10 +18,10 @@ public class AntispamCommand implements CommandExecutor {
 			if(sender.hasPermission(sky.permTurnOff) || sender.hasPermission("antispam.*") || sender.isOp()) {
 				if(sky.on) {
 					sky.on = false;
-					sender.sendMessage("§6L'antispam est désactivé");
+					sender.sendMessage("Â§6L'antispam est dÃ©sactivÃ©");
 				} else {
 					sky.on = true;
-					sender.sendMessage("§6L'antispam est activé");
+					sender.sendMessage("Â§6L'antispam est activÃ©");
 				}
 				return false;
 			}
@@ -32,35 +32,47 @@ public class AntispamCommand implements CommandExecutor {
 						try {
 							int a = Integer.parseInt(args[2]);
 							sky.getConfig().set("plugin.spamTime", a);
+							sky.saveConfig();
+							sender.sendMessage("Â§6Fait");
+							sky.loadConfig();
+							sky.listener.rl();
 							return false;
 						} catch(NumberFormatException e) {
-							sender.sendMessage("§6Please enter a valid number");
+							sender.sendMessage("Â§6Please enter a valid number");
 							return false;
 						}
 					} else if(args[1].equalsIgnoreCase("messageCooldown")) {
 						try {
 							int a = Integer.parseInt(args[2]);
 							sky.getConfig().set("plugin.messageCooldown", a);
+							sky.saveConfig();
+							sender.sendMessage("Â§6Fait");
+							sky.loadConfig();
+							sky.listener.rl();
 							return false;
 						} catch(NumberFormatException e) {
-							sender.sendMessage("§6Please enter a valid number");
+							sender.sendMessage("Â§6Please enter a valid number");
 							return false;
 						}
 					} else if(args[1].equalsIgnoreCase("messageLimite")) {
 						try {
 							int a = Integer.parseInt(args[2]);
 							sky.getConfig().set("plugin.messageLimite", a);
+							sky.saveConfig();
+							sender.sendMessage("Â§6Fait");
+							sky.loadConfig();
+							sky.listener.rl();
 							return false;
 						} catch(NumberFormatException e) {
-							sender.sendMessage("§6Please enter a valid number");
+							sender.sendMessage("Â§6Please enter a valid number");
 							return false;
 						}
 					}
 				}
-				sender.sendMessage("§6/antispam set + [args] + time");
-				sender.sendMessage("§6args = messageLimite");
-				sender.sendMessage("§6args = messageCooldown");
-				sender.sendMessage("§6args = spamTime");
+				sender.sendMessage("Â§6/antispam set + [args] + time");
+				sender.sendMessage("Â§6args = messageLimite");
+				sender.sendMessage("Â§6args = messageCooldown");
+				sender.sendMessage("Â§6args = spamTime");
 			}
 		}
 		return true;
